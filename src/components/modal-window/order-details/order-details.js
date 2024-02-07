6203bf47b8dcc685e1221fb136a0b404bf1e8a0c
Maxim Grossu <maxgrossu@yandex.ru>
2024-02-07 23:15:05 +0300

    добавлен прелоадер в модальное окно готового заказа

--- src/components/modal-window/order-details/order-details.js
+++ src/components/modal-window/order-details/order-details.js
@@ -1,17 +1,22 @@
-import React from "react";
 import { useSelector } from "react-redux";
 
 import stylesOrderDetails from './order-details.module.css';
 import doneLogo from '../../../images/done.png';
 
+import PreloaderOrderDeatails from "../../ui/preloader-order-details/preloader-order-details";
+
 function OrderDetails() {
-    const { orderDetails } = useSelector(store => ({
-        orderDetails: store.orderDetails
+    const { orderDetails, feedRequest } = useSelector(store => ({
+        orderDetails: store.orderDetails,
+        feedRequest: store.orderDetails.feedRequest
     }));
 
     return (
         <article className={ `pb-30 ${stylesOrderDetails.info}` }>
-            <h2 className={ stylesOrderDetails.price }>{ orderDetails.orderDetails && orderDetails.orderDetails.order.number }</h2>
+            { feedRequest ? 
+            (<PreloaderOrderDeatails />) :
+            <h2 className={ stylesOrderDetails.price }>{ orderDetails.orderDetails && orderDetails.orderDetails.order.number }</h2> 
+            }
             <h3 className={ stylesOrderDetails.title }>идентификатор заказа</h3>
             <img src={ doneLogo } alt='значок готово' className={ `mb-10 ${stylesOrderDetails.image}` } />
             <p className={ `text text_type_main-default mt-5 ${stylesOrderDetails.text}` }>Ваш заказ начали готовить</p>
